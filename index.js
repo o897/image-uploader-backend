ad9878c7021b3cc28621023ca783f77190c3bc52
o897 <orapelengm239@gmail.com>
2024-05-11 15:48:58 +0200

    ready for production

--- index.js
+++ index.js
@@ -1,15 +1,67 @@
 const express = require('express')
 const multer = require('multer')
 const cors = require('cors')
+const path = require('path')
 const app = express()
-const upload = multer({dest : 'uploads/'})
+const mongoose = require('mongoose')
+require('dotenv').config()
 
-app.use(cors({
-    origin: 'http://localhost:3000'
-}))
+app.use(express.static('public'))
+const storage = multer.diskStorage({
+    destination : (req,file,cb) => {
+        cb(null,'public');
+    },
+    filename: (req, file, cb) => {
+        console.log("multer : ", file.originalname);
+        cb(null, file.originalname);
+    },
+});
 
-app.get('/', (req,res) => res.send('Hello World.'))
- 
+const upload = multer({storage})
+
+// const upload = multer({
+//     dest: 'public/',
+//     filename: (req, file, cb) => {
+//         console.log("multer : ", file.originalname)
+//         cb(null, file.originalname);
+//     },
+// });
+
+// app.use(cors({
+//     origin: 'http://localhost:3000'
+// }))
+
+
+mongoose.connect(process.env["MONGO_URL"], {
+    useNewUrlParser : true,
+    useUnifiedTopology : true,
+})
+.then(() => console.log("Connected to the DB"))
+.catch((err) => console.log(err))
+// yW5PuLo9RXod6jKg
+const Schema = mongoose.Schema;
+
+// // Define our Schema
+const ImageSchema = new Schema({
+    filename : String
+})
+
+// // Collection name
+const ImageModel = mongoose.model("image",ImageSchema)
+
+app.use(cors())
+
+app.get('/', (req,res) => {
+    res.send({ message : "server running with an Usain speed"})
+})
+
+
+app.get('/api/upload/:filename', (req,res) => {
+
+    const filePath = __dirname + '/public/' + req.params.filename; // relative path
+    res.sendFile(filePath); // specify root directory
+
+})
 app.post('/upload',upload.single('file'),(req,res) => {
 
     const file = req.file;
@@ -18,8 +70,13 @@ app.post('/upload',upload.single('file'),(req,res) => {
         return res.status(400).json({error : 'No file uploaded'})
     }
 
-    const filePath = path.join(__dirname, file.path)
+    // mongoose
+    // const newImage = new ImageModel({filename : file.originalname})
 
+    // newImage.save()
+
+    const filePath = path.join(__dirname, file.path)
+    // console.log(file.originalname)
     res.json({message : 'Image uploaded successfully' , filePath})
 })
 
