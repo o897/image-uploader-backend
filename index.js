cdcc3490034bc1cf5bf1525dc858203f0849d3cb
o897 <orapelengm239@gmail.com>
2024-05-19 03:33:47 +0200

    update cors config

--- index.js
+++ index.js
@@ -6,31 +6,13 @@ const app = express();
 const fileUpload = require("./middleware/multer");
 const cloudinary = require("./utils/cloudinary");
 const streamifier = require("streamifier");
-app.use(cors());
+
+app.use(cors({
+  origin : "https://image-uploader-frontend-agg6.onrender.com/",
+  methods : ["GET","POST"],
+}));
 
 app.use(express.static("public"));
-
-// Set middleware of CORS 
-app.use((req, res, next) => {
-  res.setHeader(
-    "Access-Control-Allow-Origin",
-    "https://your-frontend.com"
-  );
-  res.setHeader(
-    "Access-Control-Allow-Methods",
-    "GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS,CONNECT,TRACE"
-  );
-  res.setHeader(
-    "Access-Control-Allow-Headers",
-    "Content-Type, Authorization, X-Content-Type-Options, Accept, X-Requested-With, Origin, Access-Control-Request-Method, Access-Control-Request-Headers"
-  );
-  res.setHeader("Access-Control-Allow-Credentials", true);
-  res.setHeader("Access-Control-Allow-Private-Network", true);
-  //  Firefox caps this at 24 hours (86400 seconds). Chromium (starting in v76) caps at 2 hours (7200 seconds). The default value is 5 seconds.
-  res.setHeader("Access-Control-Max-Age", 7200);
-
-  next();
-});
 
 const mongoose = require("./config/mongoose");
 const Schema = mongoose.Schema;
