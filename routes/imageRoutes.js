68d04ebdf8728ea4166853b3077a5847edcdec50
Orapeleng Mathebula <71043883+o897@users.noreply.github.com>
2026-01-29 08:51:23 +0000

    optimize upload api

--- routes/imageRoutes.js
+++ routes/imageRoutes.js
@@ -5,10 +5,10 @@ const fileUpload = require("../middleware/multer");
 const cloudinary = require("../utils/cloudinary");
 const streamifier = require("streamifier");
 
-const imageModel = require("../model/imageModel")
+const imageModel = require("../model/imageModel");
 
 router.get("/test", async (req, res) => {
-    res.json({error : "Image not found"});
+  res.json({ error: "Image not found" });
 });
 
 router.get("/api/upload/:filename", (req, res) => {
@@ -17,42 +17,51 @@ router.get("/api/upload/:filename", (req, res) => {
 });
 
 router.get("/api/:image", async (req, res) => {
-  const findImage = await imageModel.findOne({ filename : req.params.image });
+  const findImage = await imageModel.findOne({ filename: req.params.image });
   const results = findImage
     ? res.json(findImage)
-    : res.json({error : "Image not found"});
+    : res.json({ error: "Image not found" });
 });
 
-router.post("/upload", fileUpload.single("file"), (req, res) => {
-  const fileName = req.file.originalname;
-  
-  let streamUpload = (req) => {
-    return new Promise((resolve, reject) => {
-      let stream = cloudinary.uploader.upload_stream((error, result) => {
-        if (result) {
-          resolve(result);
-        } else {
-          reject(error);
-        }
+router.post("/upload", fileUpload.single("file"), async (req, res) => {
+  try {
+    // check to see the request has a file attached to it.
+    if (!req.file) return res.status(400).json({ error: "No file" });
+
+    const fileName = req.file.originalname;
+
+    const streamUpload = (req) => {
+      return new Promise((resolve, reject) => {
+        let stream = cloudinary.uploader.upload_stream((error, result) => {
+          if (result) {
+            resolve(result);
+          } else {
+            reject(error);
+          }
+        });
+        streamifier.createReadStream(req.file.buffer).pipe(stream);
       });
-      streamifier.createReadStream(req.file.buffer).pipe(stream);
-    });
-  };
+    };
 
-  const currentTime = new Date();
-  const timeString = currentTime.toLocaleTimeString();
-  
-  async function upload(req) {
+    const currentTime = new Date();
+    // const timeString = currentTime.toLocaleTimeString();
+    
+    // the results of how our upload went, and if its ready
     let result = await streamUpload(req);
-    // the results of how our upload went, and if its ready 
 
-    let {secure_url} = result;
-     const newImage = new imageModel({url : secure_url, filename : fileName});
-     newImage.save();
-    //  console.log(newImage);
+    let { secure_url } = result;
+    const newImage = new imageModel({ url: secure_url, filename: fileName });
+    await newImage.save();
+
+    return res.status(200).json({
+      success: true,
+      message: "Image uploaded and saved to cloudinary",
+      data: newImage,
+    });
+  } catch (err) {
+    console.log(err);
+    return res.status(500).json({ error: "Upload failed" });
   }
-  upload(req);
-  
 });
 
 module.exports = router;
