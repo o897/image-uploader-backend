9c95b75500f69d93cab52ab6384afaa2a10a6050
Orapeleng Mathebula <71043883+o897@users.noreply.github.com>
2026-02-06 09:24:05 +0000

    added codesandbox origin

--- index.js
+++ index.js
@@ -1,50 +1,54 @@
- require("dotenv").config();
+require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
 const path = require("path");
 const app = express();
-const session = require('express-session');
+const session = require("express-session");
 const passport = require("passport");
-const web = require("./routes/web")
+const web = require("./routes/web");
 // 1. Load config/Strategy
-require('./config/passport'); 
-
+require("./config/passport");
 
 const allowedOrigins = [
   "https://inquisitive-chimera-40f663.netlify.app",
   "https://image-uploader-frontend-agg6.onrender.com",
   "https://darling-ganache-c749ed.netlify.app",
   "http://localhost:3000",
-  "http://localhost:3004"
+  "http://localhost:3004",
+  "https://ttyfcy-3000.csb.app",
 ];
 
-app.use(cors({
-  origin: function (origin, callback) {
-    if (!origin || allowedOrigins.indexOf(origin) !== -1) {
-      callback(null, true);
-    } else {
-      callback(new Error('Not allowed by CORS'));
-    }
-  },
-  optionsSuccessStatus: 200
-}));
+app.use(
+  cors({
+    origin: function (origin, callback) {
+      if (!origin || allowedOrigins.indexOf(origin) !== -1) {
+        callback(null, true);
+      } else {
+        callback(new Error("Not allowed by CORS"));
+      }
+    },
+    optionsSuccessStatus: 200,
+  })
+);
 
 app.use(express.static("public"));
 
-app.use(express.json())
+app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(session({
+app.use(
+  session({
     secret: process.env.COOKIE_KEY,
     resave: false,
     saveUninitialized: false, // Don't create session until something stored
     cookie: {
-        maxAge: 24 * 60 * 60 * 1000 // 24 hours
-    }
-}));
+      maxAge: 24 * 60 * 60 * 1000, // 24 hours
+    },
+  })
+);
 
 // Initialize passport
 app.use(passport.initialize());
 app.use(passport.session());
 
-app.use('/',web)
+app.use("/", web);
 app.listen(3000, () => console.log("listening on port 3000"));
