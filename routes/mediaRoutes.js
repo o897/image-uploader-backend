c024c7bdebde019298b121f7dd0f75660d65df39
o897 <orapelengm239@gmail.com>
2026-04-17 12:27:03 +0200

    update yotube url

--- routes/mediaRoutes.js
+++ routes/mediaRoutes.js
@@ -2,27 +2,27 @@ const express = require('express');
 const router = express.Router();
 
 
-router.get("/youtube/likes", async (req,res) => {
+router.get("/youtube/likes", async (req, res) => {
     try {
 
         const token = req.user.googleAccessToken;
-        console.log("/youtube/likes route",req.user);
+        // console.log("/youtube/likes route", req.user);
 
-        const response = await fetch (
-            "https://www.googleapis.com/youtube/v3/videos?part=snippet&myRating=like&maxResults=10",
+        const response = await fetch(
+            "https://www.googleapis.com/youtube/v3/playlistItems?part=snippet&maxResults=10&playlistId=LL",
             {
-                headers : {
-                    Authorization : `Bearer ${token}`
+                headers: {
+                    Authorization: `Bearer ${token}`
                 }
             }
         );
 
         const data = await response.json();
-        console.log("youtube response",data);
-        
+        console.log("youtube response", data);
+
         res.json(data.items);
     } catch (error) {
-        res.status(500).json({error : error.message});
+        res.status(500).json({ error: error.message });
     }
 })
 
