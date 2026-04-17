57550c33183830bd54b92983c2ff01f4e533e86e
o897 <orapelengm239@gmail.com>
2026-04-17 10:50:31 +0200

    fix youtube get route

--- routes/mediaRoutes.js
+++ routes/mediaRoutes.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 
 
-router.get("youtube/likes", async (req,res) => {
+router.get("/youtube/likes", async (req,res) => {
     try {
         const token = req.user.accessToken;
 
