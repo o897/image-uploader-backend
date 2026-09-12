252b797fc79232e190c3b62b55b1d1226f46db60
Orapeleng Mathebula <71043883+o897@users.noreply.github.com>
2026-09-12 23:21:03 +0200

    Make Memoir server port configurable

--- index.js
+++ index.js
@@ -55,4 +55,6 @@ app.use(passport.initialize());
 app.use(passport.session());
 
 app.use("/", web);
-app.listen(3000, () => console.log("listening on port 3000"));
+const PORT = process.env.PORT || 3001;
+
+app.listen(PORT, () => console.log(`listening on port ${PORT}`));
